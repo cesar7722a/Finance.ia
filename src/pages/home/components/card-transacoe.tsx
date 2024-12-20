3e7c2a21e8ef6d2ce600e14e8d249ae3b990968a
César Filipe António <cesarfilipeantonio2404@gmail.com>
2024-12-20 23:27:45 +0100

    feat:add tailwind variants no card-transacoes

--- src/pages/home/components/card-transacoe.tsx
+++ src/pages/home/components/card-transacoe.tsx
@@ -1,10 +1,13 @@
 import { LucideIcon } from "lucide-react";
+import { ComponentProps } from "react";
+import { tv, VariantProps } from "tailwind-variants";
 
 interface CardTransacoesProps {
   title: string;
   data: string;
   valor: number;
   Icon: LucideIcon;
+  variantValorColor: "primary" | "secondary" | "terciary" | undefined;
 }
 
 export function CardTransacoes({
@@ -12,6 +15,7 @@ export function CardTransacoes({
   Icon,
   title,
   valor,
+  variantValorColor,
 }: CardTransacoesProps) {
   return (
     <div className="flex items-center justify-between">
@@ -26,7 +30,45 @@ export function CardTransacoes({
           </span>
         </div>
       </div>
-      <span>{valor}</span>
+      <ValorTransacoes valor={valor} colors={variantValorColor} />
     </div>
+  );
+}
+
+const spanVariants = tv({
+  base: "font-bold text-sm",
+  variants: {
+    colors: {
+      primary: "text-white",
+      secondary: "text-redPrimary-1",
+      terciary: "text-brandPrimary-1",
+    },
+    size: {
+      sm: "text-sm",
+      md: "text-base",
+      lg: "px-4 py-3 text-lg",
+    },
+  },
+  defaultVariants: {
+    size: "sm",
+    color: "primary",
+  },
+});
+
+interface NewProps
+  extends ComponentProps<`span`>,
+    VariantProps<typeof spanVariants> {
+  valor: number;
+}
+
+export function ValorTransacoes({ valor, colors, ...props }: NewProps) {
+  return colors === "primary" || colors === "secondary" ? (
+    <span {...props} className={spanVariants({ colors })}>
+      -{valor} Kz
+    </span>
+  ) : (
+    <span {...props} className={spanVariants({ colors })}>
+      +{valor} Kz
+    </span>
   );
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -152,7 +152,7 @@ export function Home() {
               </div>
             </div>
           </div>
-          <div className="w-[444px] border border-white/10 rounded-xl p-5 space-y-6">
+          <div className="w-[444px] border border-white/10 rounded-xl p-6 space-y-5">
             <div className="flex justify-between items-center">
               <h4 className="text-lg font-bold">Transações</h4>
               <Button className="rounded-full border border-white/10 bg-transparent hover:bg-white/5">
@@ -161,22 +161,84 @@ export function Home() {
             </div>
             <div className="h-px bg-white/10" />
             <CardTransacoes
-              Icon={CreditCard}
+              Icon={Component}
               data="25 Nov, 2025"
               title="Salário"
               valor={3900}
+              variantValorColor="terciary"
             />
+            <CardTransacoes
+              Icon={Component}
+              data="25 Nov, 2025"
+              title="Bitcoin"
+              valor={3900}
+              variantValorColor="primary"
+            />
+            <CardTransacoes
+              Icon={CreditCard}
+              data="25 Nov, 2025"
+              title="Academia"
+              valor={3900}
+              variantValorColor="secondary"
+            />
+            <CardTransacoes
+              Icon={Component}
+              data="25 Nov, 2025"
+              title="Aluguel"
+              valor={3900}
+              variantValorColor="secondary"
+            />
+
             <CardTransacoes
               Icon={Barcode}
               data="25 Nov, 2025"
-              title="Salário"
+              title="Freelancing"
               valor={3900}
+              variantValorColor="terciary"
             />
+            <CardTransacoes
+              Icon={Component}
+              data="25 Nov, 2025"
+              title="Freelancing"
+              valor={3900}
+              variantValorColor="terciary"
+            />
+
             <CardTransacoes
               Icon={Component}
               data="25 Nov, 2025"
               title="Salário"
               valor={3900}
+              variantValorColor="primary"
+            />
+            <CardTransacoes
+              Icon={Component}
+              data="25 Nov, 2025"
+              title="Bitcoin"
+              valor={3900}
+              variantValorColor="primary"
+            />
+            <CardTransacoes
+              Icon={CreditCard}
+              data="25 Nov, 2025"
+              title="Academia"
+              valor={3900}
+              variantValorColor="secondary"
+            />
+            <CardTransacoes
+              Icon={Component}
+              data="25 Nov, 2025"
+              title="Aluguel"
+              valor={3900}
+              variantValorColor="secondary"
+            />
+
+            <CardTransacoes
+              Icon={Barcode}
+              data="25 Nov, 2025"
+              title="Freelancing"
+              valor={3900}
+              variantValorColor="terciary"
             />
           </div>
         </div>
